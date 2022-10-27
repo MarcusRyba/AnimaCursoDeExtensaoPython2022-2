c076bb4d7ae02a17c09334fbd635637bc3f88a74
MarcusRyba <116229011+MarcusRyba@users.noreply.github.com>
2022-10-27 01:11:00 +0000

    Aula 2

--- main.py
+++ main.py
@@ -11,3 +11,27 @@ nome = "Marcus ryba"
 idade = 19 
 
 print(nome)
+
+print("Alo mundo!")
+
+
+print("Minha idade é "+str(idade)+" anos")
+print(f"Minha idade é {idade} anos")
+print("Minha idade é {} anos".format(idade))
+
+print("Meu nome é {} e tenho {} anos".format(nome, idade))
+dobro = idade * 2
+print("O dobro da idade informada é {}".format(dobro))
+
+if idade >= 18:
+  print("Você é maior de idade, ótimo! Já pode beber ou dirigir")
+else:
+  print("Você é xóven ainda, xóven ainda...")
+
+genero = input("Informe o gênero M=Masculino, F=Feminino, O=Outros: ")
+if idade >= 18 and genero == "M":
+  print("... e você também precisa/precisou prestar o serviço militar obrigatório")
+
+
+
+print("vai ser executada de qualquer jeito")
